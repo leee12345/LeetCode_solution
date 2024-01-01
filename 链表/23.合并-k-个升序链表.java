473442bcb628956908a9b20a88749be593da308d
15116 <15116908166@163.com>
2024-01-01 14:36:14 +0800

    1.1

--- 链表/23.合并-k-个升序链表.java
+++ 链表/23.合并-k-个升序链表.java
@@ -74,25 +74,27 @@
 class Solution {
     //顺序合并
     public ListNode mergeKLists(ListNode[] lists) {
-       ListNode ans=null;
-       for(int i=0;i<lists.length;i++){
-        ans=mergeTwoList(ans, lists[i]);
-       }
-       return ans;
+        ListNode ans=null;
+        for(int i=0;i<lists.length;i++){
+            ans=mergeTwoList(ans,lists[i]);
+        }
+        return ans;
     }
-    private ListNode mergeTwoList(ListNode list1,ListNode list2){
-        if(list1==null){
-            return list2;
-        }else if(list2==null){
-            return list1;
-        }else if(list1.val<list2.val){
-            list1.next=mergeTwoList(list1.next,list2);
-            return list1;
+
+    private ListNode mergeTwoList(ListNode l1,ListNode l2){
+        if(l1==null){
+            return l2;
+        }else if(l2==null){
+            return l1;
+        }else if(l1.val<l2.val){
+            l1.next=mergeTwoList(l1.next, l2);
+            return l1;
         }else{
-            list2.next=mergeTwoList(list1,list2.next);
-            return list2;
+            l2.next=mergeTwoList(l1, l2.next);
+            return l2;
         }
     }
+
 }
 // @lc code=end
 
