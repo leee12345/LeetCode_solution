68ab4b9eb015f4c0d16e08664c1744ebc84446e1
15116 <15116908166@163.com>
2024-01-24 21:17:44 +0800

    1.24

---
+++ hot 100/102.二叉树的层序遍历.java
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=java
+ *
+ * [102] 二叉树的层序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (66.26%)
+ * Likes:    1864
+ * Dislikes: 0
+ * Total Accepted:    936.8K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[[1]]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 2000] 内
+ * -1000 <= Node.val <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+
+/***
+ * BFS
+ * 时间复杂度 O(n)
+ * 空间复杂度 O(n)
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res= new ArrayList<List<Integer>>();
+        if(root==null){
+            return res;
+        }
+        Queue<TreeNode> queue= new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            List<Integer> level = new ArrayList<Integer>();
+            int currentLevelSize = queue.size();
+            for(int i=1;i<=currentLevelSize;i++){
+                TreeNode node=queue.poll();
+                level.add(node.val);
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+                if(node.right!=null){
+                    queue.offer(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
