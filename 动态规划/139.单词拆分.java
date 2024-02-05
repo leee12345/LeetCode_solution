78c123fed056768d1364b9823aef4ef2ebe6dff4
15116 <15116908166@163.com>
2024-02-05 23:05:25 +0800

    2.5

---
+++ 动态规划/139.单词拆分.java
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=java
+ *
+ * [139] 单词拆分
+ *
+ * https://leetcode.cn/problems/word-break/description/
+ *
+ * algorithms
+ * Medium (54.68%)
+ * Likes:    2413
+ * Dislikes: 0
+ * Total Accepted:    526.7K
+ * Total Submissions: 959.9K
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。如果可以利用字典中出现的一个或多个单词拼接出 s 则返回 true。
+ * 
+ * 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+ * 注意，你可以重复使用字典中的单词。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= s.length <= 300
+ * 1 <= wordDict.length <= 1000
+ * 1 <= wordDict[i].length <= 20
+ * s 和 wordDict[i] 仅由小写英文字母组成
+ * wordDict 中的所有字符串 互不相同
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.HashSet;
+
+/**
+ * HashSet
+ * 
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>(wordDict);
+        boolean[] valid = new boolean[s.length()+1];
+        valid[0]=true;
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<i&&!valid[i];j++){
+                if(set.contains(s.substring(j,i))&&valid[j]){
+                    valid[i]=true;
+                }
+            }
+        }
+        return valid[s.length()];
+    }
+}
+ */
+/**
+ * dp
+ */
+class Solution {
+    // 另一种思路的背包
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean []dp =new boolean[s.length()+1];
+        dp[0]=true;
+        for(int i=1;i<=s.length();i++)
+        {
+            for(String word:wordDict){
+                int len=word.length();
+                if(i>=len && dp[i-len]&&word.equals(s.substring(i-len,i)))
+                {
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+
+    }
+}
+// @lc code=end
+
--- 动态规划/322.零钱兑换.java
+++ 动态规划/322.零钱兑换.java
@@ -56,7 +56,20 @@
 // @lc code=start
 class Solution {
     public int coinChange(int[] coins, int amount) {
-
+        int[] dp  = new int[amount+1];
+        int max=Integer.MAX_VALUE;
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            dp[i]=max;
+        }
+        for(int i=0;i<coins.length;i++){
+            for(int j=coins[i];j<=amount;j++){
+                if(dp[j-coins[i]]!=max){
+                    dp[j]=Math.min(dp[j],dp[j-coins[i]]+1);
+                }
+            }
+        }
+        return dp[amount]==max?-1:dp[amount];
     }
 }
 // @lc code=end
